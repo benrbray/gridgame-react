7fd336971cf4989b830da9f338a97c569a050506
Benjamin R. Bray <benrbray@umich.edu>
2020-03-25 22:08:01 -0400

    initialize typescript+react+parcel project

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+function Welcome() {
+	return <h1>Hello, World!</h1>;
+}
+
+ReactDOM.render(
+	<Welcome />,
+	document.getElementById("root")
+);
